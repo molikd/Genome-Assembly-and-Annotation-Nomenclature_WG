9bb988684b415dea505cbf2ea0280abd6d5a2155
molikd <david.molik@usda.gov>
2023-07-13 16:04:48 -0500

    intial skeleton

---
+++ lib/gaan/__init__.py
@@ -0,0 +1,1 @@
+from .gaan import gaan
---
+++ lib/gaan/gaan.py
@@ -0,0 +1,14 @@
+import re
+import json
+from jsonschema import validate
+
+schema = {
+}
+
+class gaan:
+
+    def __init__():
+
+    def validate(self):
+        gaan_instance = json.dumps(self.__dict__)
+        validate(instance=gaan_instance, schema=schema)
